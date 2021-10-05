cb3cb72435d1c151b4efae3390fa8d07fdce6f08
DamianSartorio <damiansartorio@gmail.com>
2021-10-05 18:00:01 -0300

    comit con clases

---
+++ Classes/Users/Person.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Classes\Users;
+
+/// Atributos
+
+abstract class Person
+{
+    private $name;
+    private $lastName;
+    private $dni;
+    private $gender;
+    private $birthDate;
+    private $email;
+    private $phoneNumber;
+}
+
+/// Getters y Setters
