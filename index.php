049ab3ce363abc739595d9fe8da1aa92dc0fa7a6
PilarLastra <60575602+PilarLastra@users.noreply.github.com>
2021-10-17 16:57:36 -0300

    Pantalla de log in

--- index.php
+++ index.php
@@ -2,17 +2,41 @@
 <html lang="en">
 
 <head>
+<link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<script href="lib/bootstrap/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="Css.css" type="text/css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<body style = "background-color:#7B68EE">
+<span><img src="imagenes/logo UTN con UTN.png" class="logo" alt=""></span>
 
-<body>
-    <?php echo "awante mi branch";  ?>
-    <?php echo "awante mi branch";  ?>
-   
+<div class="container">
+  <div class="row align-items-center">
+    <div class="col"> 
 
+    </div>
+    <div class="col" id="cuadrado">
+        
+        <header > <p>Iniciar Sesión<p> <header>
+        <header id= "linea"><header >
+        <input type="email" class="field field--animate" id="emailadress" placeholder="Email:">
+
+        <button type="submit" class="buttonFlecha"><img src="imagenes/boton violeta de inicio.png" class="boton" alt=""></button>
+
+        <button type="submit" class="buttonCampus"><img src="imagenes/utn campus boton.png" class="botonCampus" alt=""></button>
+        <button type="submit" class="buttonIg"><img src="imagenes/instagaram boton .png" class="botonInstagram" alt=""></button>
+        <button type="submit" class="buttonTwitter"><img src="imagenes/twitter boton.png" class="botonTwitter" alt=""></button>
+        
+
+    </div>
+    <div class="col">
+  
+  
+</div>
+    
+    
 </body>
-
 </html>
