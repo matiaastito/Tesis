bc4d03d5af769ef68340cc6dd874ebe65749901f
PilarLastra <60575602+PilarLastra@users.noreply.github.com>
2021-11-02 15:28:34 -0300

    Cambios con el profe

--- perfil.php
+++ perfil.php
@@ -2,8 +2,9 @@
 <html lang="en">
 
 <head>
-<link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-<script href="lib/bootstrap/js/bootstrap.min.js"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+<link rel="stylesheet" href="overides.css" type="text/css">
 <link rel="stylesheet" href="perfil.css" type="text/css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,127 +13,126 @@
 </head>
 <body style = "background-color:#7B68EE">
 
-<header>
-
-    <div class="px-3 py-2 bg-dark text-white">
-      <div class="container">
-        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-          <a href="/" class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
-            <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
-          </a>
-
-          <! –– Barra de arriba ––>
-          <li>
-              <a href="#" class="container d-flex flex-wrap justify-content-center">
-                <form action="" class="col-12 col-lg-auto mb-2 mb-lg-0 me-lg-auto">
-                <input type="submit" class="logo" value="">
-              </a>
-            </li>
-          <li>
-              <a href="#" class="container d-flex flex-wrap justify-content-center">
-                <form class="col-12 col-lg-auto mb-2 mb-lg-0 me-lg-auto">
-                <input type="search" class="searchBar" placeholder="Buscar..." aria-label="Search">
-              </a>
-            </li>
-          <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
-            <li>
-              <a href="#" class="nav-link text-white">
-                <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#home"/></svg>
-                Propuestas
-              </a>
-            </li>
-            <li>
-              <a href="#" class="nav-link text-white">
-                <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#speedometer2"/></svg>
-                Notificaciones
-              </a>
-            </li>
-            <li>
-              <a href="#" class="nav-link text-white">
-                <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#grid"/></svg>
-                Mi cuenta
-              </a>
-            </li>
-            <li>
-              <a href="#" class="nav-link text-white">
-                <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#grid"/></svg>
-                LogOut
-              </a>
-            </li>
-            
-          </ul>
-        </div>
-      </div>
-    </div>
-
-    <! –– Parte superior del perfil ––>
-
-    <div class="header">
-      <div>
-        <img class="foto" src="imagenes/foto default de usuario.png" alt="">
-        <h1>Nombre Apellido</h1>
-      </div>
-      <hr>
-       <nav>
-       <ul class="opcionesDePerfil">
-            <li>
-                <a href="#" class="nav-link text-white">
-                    <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#people-circle"/></svg>
-                   Ver CV
-              </a>
-            </li>
-            <li>
-                <a href="#" class="nav-link text-white">
-                    <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#people-circle"/></svg>
-                   GitHub
-              </a>
-            <li>
-                <a href="#" class="nav-link text-white">
-                    <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#people-circle"/></svg>
-                   Informacion
-              </a>
-            </li>
-            <li>
-                <a href="#" class="nav-link text-white">
-                    <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#people-circle"/></svg>
-                   Imagenes
-              </a>
-            </li>
-            <li>
-                <a href="#" class="nav-link text-white">
-                    <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#people-circle"/></svg>
-                   Videos
-              </a>
-            </li>
-            </ul>
-       </nav>
-            
+<header> 
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="#"><img src="imagenes/logoNegro.png" alt=""></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Propuestas</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Notificaciones</a>
+          </li>
           
-       
-
-         <! –– Parte inferior del perfil (Muro de publicaciones) ––>
+          <li class="nav-item">
+            <a class="nav-link">Mi Cuenta</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link">LogOut</a>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
     </div>
+  </nav>
 
-    <div contenteditable class="caja" oninput="document.querySelector('#description').textContent = this.innerText"></div>
-            
-    <ul>
+</header>
+
+<main>
+  <div class="container">
+    <div class="cover-image">
+      <button class="btn" type="button" data-bs-toggle="modal" data-bs-target="#update-cover-image-modal">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
+      </button>
+    
+      <!–– Imagen de Portada ––>
+    </div>
+    <div class="profile-wrapper">
+          <img class="foto" src="imagenes/foto default de usuario.png" alt="">
+          <h1 class="userName">Nombre Apellido</h1>
+          <h2 class="linea"></h2>
+      </div>
+    <nav class="mb-5">
+      <ul class="nav justify-content-center">
+          <li class="nav-item">
+              <a href="#" class="nav-link text-white">
+                  <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#people-circle"/></svg>
+                  Ver CV
+            </a>
+          </li>
+          <li class="nav-item">
+              <a href="#" class="nav-link text-white">
+                  <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#people-circle"/></svg>
+                  GitHub
+            </a>
+          <li class="nav-item">
+              <a href="#" class="nav-link text-white">
+                  <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#people-circle"/></svg>
+                  Informacion
+            </a>
+          </li>
+          <li class="nav-item">
+              <a href="#" class="nav-link text-white">
+                  <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#people-circle"/></svg>
+                  Imagenes
+            </a>
+          </li>
+          <li class="nav-item">
+              <a href="#" class="nav-link text-white">
+                  <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#people-circle"/></svg>
+                  Videos
+            </a>
+          </li>
+          </ul>
+      </nav>
+
+      <section >
+        <form action="">
+        <div class="mb-3">
+           <label for="exampleFormControlTextarea1" class="form-label"></label>
+           <textarea class="caja_de_texto" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+          <button type="submit" class="button-agregar">Agregar</button>
+        </form>
+      </section>
+  </div>
+  
+
+</main>
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="update-cover-image-modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
         
-    </ul>
-    <ul>
-          <h4 class= "lineaDelMuroDeTexto"></h4>
-          <button type="submit" class="agregar" value="Agregar">Agregar</button>
-    </ul>
-    <ul>
-         <h5 class="cajaMuro"> </h5>
+            <!-- Form para actializarb la imagen -->
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+   
 
-    </ul>
-            
-
-
-
-  </header>
-    
-    
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
 
